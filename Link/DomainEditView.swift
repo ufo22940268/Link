cc16fe882fca231adede5738980b84b92a14b225
Frank Cheng <ufo22940268@gmail.com>
2020-06-27 07:14:52 +0800

    v

--- Link/DomainEditView.swift
+++ Link/DomainEditView.swift
@@ -8,24 +8,50 @@
 
 import SwiftUI
 
+extension String {
+    
+    func isValidURL() -> Bool {
+        guard !contains("..") else { return false }
+        
+        let head     = "((http|https)://)?([(w|W)]{3}+\\.)?"
+        let tail     = "\\.+[A-Za-z]{2,3}+(\\.)?+(/(.)*)?"
+        let urlRegEx = head+"+(.)+"+tail
+        
+        let urlTest = NSPredicate(format:"SELF MATCHES %@", urlRegEx)
+        return urlTest.evaluate(with: trimmingCharacters(in: .whitespaces))
+    }
+}
 
 struct DomainEditView: View {
     
     @State var domainUrl: String = ""
     @State var domainName: String = ""
-
+    
+    var nextButton: some View {
+        Button(action: {
+        }) {
+            Text("下一步")
+        }.disabled(!isFormValid)
+    }
+    
+    var isFormValid: Bool {
+        return domainUrl.isValidURL()
+    }
+    
+    
+    
     var body: some View {
         Form {
             Section(header: Text("")) {
                 HStack {
                     Text("域名地址")
                     Spacer()
-                    TextField("example.com", text: $domainUrl, onEditingChanged: { b in
+                    TextField("https://example.com", text: $domainUrl, onEditingChanged: { b in
                         guard !b else { return }
                         if self.domainName == "" {
                             self.domainName = extractDomainName(fromURL: self.domainUrl)
                         }
-                    }).multilineTextAlignment(.trailing).textContentType(.URL).keyboardType(.URL)
+                    }).multilineTextAlignment(.trailing).textContentType(.URL).keyboardType(.URL).autocapitalization(.none)
                 }
                 HStack {
                     Text("名字")
@@ -33,15 +59,20 @@ struct DomainEditView: View {
                     TextField("example", text: $domainName).multilineTextAlignment(.trailing)
                 }
             }
-        }
+        }.navigationBarTitle("输入域名", displayMode: .inline)
+            .navigationBarItems(trailing: nextButton)
     }
 }
 
 struct DomainEditView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            DomainEditView()
-            DomainEditView().colorScheme(.dark)
+            NavigationView {
+                DomainEditView()
+            }
+            NavigationView {
+                DomainEditView()
+            }.colorScheme(.dark)
         }
     }
 }
--- Link/Tools.swift
+++ Link/Tools.swift
@@ -9,5 +9,11 @@
 import Foundation
 
 func extractDomainName(fromURL:  String) -> String {
-    return "ii"
+    let regex = try? NSRegularExpression(pattern: "((http|https)://)?(\\w+\\.)?(?<dn>(\\w)+)\\.", options: [])
+    if let match = regex?.firstMatch(in: fromURL, options: [], range: NSRange(location: 0, length: fromURL.utf16.count)) {
+        if let domainNameRange = Range(match.range(withName: "dn"), in: fromURL)  {
+            return String(fromURL[domainNameRange])
+        }
+    }
+    return ""
 }
