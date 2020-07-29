6f3007d4f664ed9df9aa9439b24c93ca8f089f02
Frank Cheng <ufo22940268@gmail.com>
2020-07-29 16:52:28 +0800

    v

--- Link/EndPointEditView.swift
+++ Link/EndPointEditView.swift
@@ -8,37 +8,55 @@
 
 import SwiftUI
 import Combine
- 
+
 struct EndPointEditListItemView: View {
     
-    var api: Api
-    
+    @Binding var api: Api
     @Environment(\.editMode) var mode
+    
+    var isEditing: Bool {
+        self.mode != nil && self.mode!.wrappedValue.isEditing
+    }
+    
+    var innerBody: some View {
+        Text(api.paths.last ?? "")
+    }
+    
     var body: some View {
         NavigationLink(destination: EmptyView()) {
-            Text((self.mode != nil && self.mode!.wrappedValue.isEditing) ? "active" : "inactive")
+            innerBody
         }
+        
+        //Animation wired.
+//        if isEditing {
+//            return AnyView(innerBody)
+//        } else {
+//            return AnyView(NavigationLink(destination: EmptyView()) {
+//                innerBody
+//            })
+//        }
     }
 }
 
 struct EndPointEditListView: View {
-            
+    
     @State var apis = [Api]()
     @State private var c : AnyCancellable?
+    @State  var selection = Set<Int>()
     @Environment(\.editMode) var mode
-
+    
     fileprivate func loadData() {
         self.c = ApiHelper().fetch()
             .catch { error in
                 return Just([])
-            }
-            .receive(on: DispatchQueue.main)
-            .assign(to: \EndPointEditListView.apis, on: self)
+        }
+        .receive(on: DispatchQueue.main)
+        .assign(to: \EndPointEditListView.apis, on: self)
     }
     
     var body: some View {
-        List(0..<apis.count, id: \.self) { (i: Int) in
-            EndPointEditListItemView(api: self.apis[i])
+        List(0..<apis.count, id: \.self, selection: $selection) { (i: Int) in
+            EndPointEditListItemView(api: self.$apis[i])
                 .environment(\.editMode, self.mode)
         }
         .onAppear {
@@ -60,4 +78,4 @@ struct EndPointEditView_Previews: PreviewProvider {
         }
     }
 }
- 
+
