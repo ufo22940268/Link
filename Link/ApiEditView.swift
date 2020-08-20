9116825a98907d27516828b8bbdfb30515239bbf
Frank Cheng <ufo22940268@gmail.com>
2020-08-20 09:25:26 +0800

    v

--- Link/ApiEditView.swift
+++ Link/ApiEditView.swift
@@ -45,6 +45,7 @@ struct ApiEditView: View {
     @ObservedObject var apiEditData: ApiEditData
     @EnvironmentObject var domainData: DomainData
     @Binding var dismissPresentationMode: PresentationMode?
+    @State var segmentSelection = Segment.all.rawValue
 
     var doneButton: some View {
         Button("完成", action: {
@@ -53,18 +54,68 @@ struct ApiEditView: View {
         })
     }
 
-    var body: some View {
-        List {
-            ForEach(self.apiEditData.apis.indices, id: \.self) { i in
-                ApiEditListItemView(api: self.$apiEditData.apis[i], selected: self.apiEditData.apis[i].watch)
+    enum Segment: Int, RawRepresentable, CaseIterable {
+        case all = 0
+        case watch = 1
+
+        var label: String {
+            switch self {
+            case .all:
+                return "全部"
+            case .watch:
+                return "已关注"
             }
         }
-        .navigationBarItems(trailing: doneButton)
+    }
+
+    var categorySelectorView: some View {
+        Picker("Select api category", selection: $segmentSelection) {
+            ForEach(Segment.allCases, id: \.self.rawValue) { segment in
+                Text(segment.label).tag(segment.rawValue)
+            }
+        }.pickerStyle(SegmentedPickerStyle()).fixedSize().padding()
+    }
+
+    var categoryApis: [ApiEntity] {
+        if segmentSelection == Segment.all.rawValue {
+            return apiEditData.apis
+        } else {
+            return apiEditData.apis.filter { $0.watch }
+        }
+    }
+
+    func getApiBinding(_ api: ApiEntity) -> Binding<ApiEntity> {
+        let index = apiEditData.apis.firstIndex(of: api)!
+        return $apiEditData.apis[index]
+    }
+
+    var body: some View {
+        VStack {
+            categorySelectorView
+
+            List {
+                ForEach(self.categoryApis, id: \.self) { api in
+                    ApiEditListItemView(api: self.getApiBinding(api), selected: api.watch)
+                }
+            }
+        }.navigationBarItems(trailing: doneButton).navigationBarTitle("字段")
     }
 }
 
 struct ApiEditView_Previews: PreviewProvider {
     static var previews: some View {
-        ApiEditView(apiEditData: ApiEditData(), dismissPresentationMode: Binding.constant(nil))
+        let d = ApiEditData()
+        let a = ApiEntity(context: context)
+        a.paths = "wefwef"
+        a.watch = true
+        let a2 = ApiEntity(context: context)
+        a2.paths = "wefwef2"
+        a2.watch = false
+        d.apis = [a, a2]
+        return NavigationView {
+            ApiEditView(apiEditData: d, dismissPresentationMode: Binding.constant(nil))
+                .environment(\.managedObjectContext, context)
+                .colorScheme(.dark)
+        }
     }
 }
