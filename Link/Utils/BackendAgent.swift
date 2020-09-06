edf62d63b3ae353097bfa3723f554a15404f569e
Frank Cheng <ufo22940268@gmail.com>
2020-09-06 09:43:06 +0800

    v

--- Link/Utils/BackendAgent.swift
+++ Link/Utils/BackendAgent.swift
@@ -1,7 +1,7 @@
 //
 //  BackendAgent.swift
 //  Link
-// 
+//
 //  Created by Frank Cheng on 2020/9/6.
 //  Copyright © 2020 Frank Cheng. All rights reserved.
 //
@@ -15,6 +15,21 @@ class BackendAgent {
 
     typealias Response = JSON
 
+    struct ResponseError: Error {
+        internal init(json: JSON? = nil) {
+            self.json = json
+        }
+
+        internal init(error: Error) {
+            self.error = error
+        }
+
+        var json: JSON?
+        var error: Error?
+
+        static let parseError = ResponseError()
+    }
+
     init() {
         if let loginInfo = LoginStore.getLoginInfo() {
             self.loginInfo = loginInfo
@@ -23,19 +38,28 @@ class BackendAgent {
         }
     }
 
-    private func post(endPoint: String, data: [String: Any]) throws -> AnyPublisher<Response, Never> {
+    private func post(endPoint: String, data: [String: Any]) throws -> AnyPublisher<Response, BackendAgent.ResponseError> {
         let url = (URL(string: Self.backendDomain)?.appendingPathComponent(endPoint))!
         var req = URLRequest(url: url)
         req.httpMethod = "POST"
         req.httpBody = try JSONSerialization.data(withJSONObject: data, options: [])
         req.setValue("application/json", forHTTPHeaderField: "Content-Type")
         return URLSession.shared.dataTaskPublisher(for: req)
-            .tryMap { (data, _) throws in
-                let json = try JSON(data: data)
-                return json
+            .tryMap { (data, _) throws -> JSON in
+                if let json = try? JSON(data: data) {
+                    if json["ok"].bool == true {
+                        return json
+                    }
+                    throw ResponseError(json: json)
+                }
+                throw ResponseError.parseError
             }
-            .catch { _ in
-                Empty()
+            .mapError { (e) -> ResponseError in
+                if let e = e as? ResponseError {
+                    return e
+                } else {
+                    return ResponseError(error: e)
+                }
             }
             .eraseToAnyPublisher()
     }
@@ -43,6 +67,7 @@ class BackendAgent {
     func login(loginInfo: LoginInfo) -> AnyPublisher<Void, Never> {
         try! self.post(endPoint: "/user/login", data: ["appleUserId": "123"])
             .map { _ in () }
+            .replaceError(with: ())
             .eraseToAnyPublisher()
     }
 }
