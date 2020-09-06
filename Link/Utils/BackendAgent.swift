b2be037314d230722af4b47fb9f629fff2c13acb
Frank Cheng <ufo22940268@gmail.com>
2020-09-06 08:31:01 +0800

    vvv

--- Link/Utils/BackendAgent.swift
+++ Link/Utils/BackendAgent.swift
@@ -1,7 +1,7 @@
 //
 //  BackendAgent.swift
 //  Link
-//
+// 
 //  Created by Frank Cheng on 2020/9/6.
 //  Copyright © 2020 Frank Cheng. All rights reserved.
 //
@@ -13,6 +13,8 @@ class BackendAgent {
     static let backendDomain = "http://biubiubiu.hopto.org:3000"
     var loginInfo: LoginInfo
 
+    typealias Response = JSON
+
     init() {
         if let loginInfo = LoginStore.getLoginInfo() {
             self.loginInfo = loginInfo
@@ -21,15 +23,16 @@ class BackendAgent {
         }
     }
 
-    private func post(endPoint: String, data: [String: Any]) throws -> AnyPublisher<Void, Never> {
+    private func post(endPoint: String, data: [String: Any]) throws -> AnyPublisher<Response, Never> {
         let url = (URL(string: Self.backendDomain)?.appendingPathComponent(endPoint))!
         var req = URLRequest(url: url)
         req.httpMethod = "POST"
         req.httpBody = try JSONSerialization.data(withJSONObject: data, options: [])
         req.setValue("application/json", forHTTPHeaderField: "Content-Type")
         return URLSession.shared.dataTaskPublisher(for: req)
-            .map { _ in
-                ()
+            .tryMap { (data, _) throws in
+                let json = try JSON(data: data)
+                return json
             }
             .catch { _ in
                 Empty()
@@ -39,5 +42,7 @@ class BackendAgent {
 
     func login(loginInfo: LoginInfo) -> AnyPublisher<Void, Never> {
         try! self.post(endPoint: "/user/login", data: ["appleUserId": "123"])
+            .map { _ in () }
+            .eraseToAnyPublisher()
     }
 }
