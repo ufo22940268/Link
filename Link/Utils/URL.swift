90cd612bb059fcded9454badc7aead8f6c6ba092
Frank Cheng <ufo22940268@gmail.com>
2020-09-15 07:54:28 +0800

    v

--- Link/Utils/URL.swift
+++ Link/Utils/URL.swift
@@ -61,18 +61,22 @@ extension JSONDecoder {
 }
 
 extension Publisher {
-    func parseArrayObjects<T>(to entity: T.Type) -> Publishers.Map<Self, [T]> where Self.Output == JSON, T: Codable {
+    func parseArrayObjects<T>(to entity: T.Type) -> AnyPublisher<[T], Self.Failure> where Self.Output == JSON, T: Codable {
         Publishers.Map(upstream: self) { json in
             json.result.arrayValue.map { json -> T in
                 try! JSONDecoder.backendDecoder.decode(entity, from: json.rawData())
             }
         }
+        .receive(on: DispatchQueue.main)
+        .eraseToAnyPublisher()
     }
 
-    func parseObject<T>(to entity: T.Type) -> Publishers.Map<Self, T> where Self.Output == JSON, T: Codable {
+    func parseObject<T>(to entity: T.Type) -> AnyPublisher<T, Self.Failure> where Self.Output == JSON, T: Codable {
         Publishers.Map(upstream: self) { json in
             try! JSONDecoder.backendDecoder.decode(entity, from: json.rawData())
         }
+        .receive(on: DispatchQueue.main)
+        .eraseToAnyPublisher()
     }
 }
 
